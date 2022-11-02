06d5f34a0b28f34983ce936361af9c5a1bac91e8
juan pablo <jhonpol2016@icloud.com>
2022-11-02 14:42:35 -0300

    cambios

--- codo a codo/js/tickets.js
+++ codo a codo/js/tickets.js
@@ -1,12 +1,21 @@
-
-
-
-
     const precio = 200;
-    let precio_final
+    let precio_final;
+
+    let cantidad = document.getElementById("cantidad");
+
+    cantidad.addEventListener("change",input)
+    function input(){
+        let valor = cantidad.value;
+        if (valor == null) {
+            valor = 1;
+        }
+        return valor;
+    }
+
 
     categorias.addEventListener("change", descuentos);
     function descuentos(){
+        
         let categorias = document.getElementById("categorias").value;
         switch (categorias) {
             case "Estudiante":precio_final = precio - (precio * 0.8);
@@ -17,12 +26,12 @@
             break;
             case"Sin_categoria": precio_final = precio;
             break;
-        }            
+        }         
         boton2.addEventListener("click", resume);   
         function resume(){
             let boton2 = document.getElementById("boton2");
- 
-            document.getElementById("total").innerText = `Total a pagar: $${precio_final}`;
+            total = precio_final * input();
+            document.getElementById("total").innerText = `Total a pagar: $${total}`;
         }
         boton1.addEventListener("click", borrar);   
         function borrar(){
